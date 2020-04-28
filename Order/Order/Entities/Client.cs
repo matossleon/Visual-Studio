b49ae6bfe0b1f5c7dbdba1da06dead9cee2b69a0
Leonardo M. Silva <leonardoms171@gmail.com>
2020-04-28 14:11:56 -0300

    Order - Entities

---
+++ Order/Order/Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Entities
+{
+    class Client
+    {
+    }
+}
---
+++ Order/Order/Entities/Enuns/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Entities.Enuns
+{
+    class OrderStatus
+    {
+    }
+}
